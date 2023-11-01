d359d6ebc7904be8a53f1a215b56c66d05a2a29c
Gabriel Ocampos <gabriel1234511@hotmail.com>
2023-10-31 21:34:32 -0300

    hasheo completado

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,22 +1,38 @@
 import { Router } from "express";
+import { HashData, CompareData } from "../utils.js";
 import { usersManager } from "../dao/mongo/UserManager.js";
 const UserRouter = Router();
 
 UserRouter.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const userDB = await usersManager.findByEmail(email);
+
     if (!userDB) { return res.json({ error: "Verifique los datos ingresados" }) }
-    req.session["email"] = email;
-    req.session["first_name"] = userDB.first_name;
-    req.session['last_name'] = userDB.last_name
-    req.session['isAdmin'] =
-        email === "adminCoder@coder.com" && password === "Cod3r123" ? "admin" : "User"
-    res.redirect("/views/products");
+    // req.session["email"] = email;
+    // req.session["first_name"] = userDB.first_name;
+    // req.session['last_name'] = userDB.last_name
+    // req.session['isAdmin'] =
+    //     email === "adminCoder@coder.com" && password === "Cod3r123" ? "admin" : "User"
+    // res.redirect("/views/products");
+
+    const isValid = await CompareData(password, userDB.password)
+    if (!isValid) {
+        return res.status(401).json({ message: 'Verifique los datos ingresados' })
+    } else {
+        res.redirect('/views/products')
+    }
 });
 
 UserRouter.post("/signup", async (req, res) => {
-    console.log(req.body)
-    const createdUser = await usersManager.createOne(req.body);
-    res.status(200).json({ message: "User created", createdUser });
+    const { password } = req.body
+    try {
+        const HashedPass = await HashData(password)
+        const createdUser = await usersManager.createOne({
+            ...req.body,
+            password: HashedPass
+        });
+        res.status(200).json({ message: "User created", createdUser });
+    } catch (error) { return res.status(400).json(error) }
+
 });
 export default UserRouter;
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,14 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bcrypt from 'bcrypt'
+
 
 export const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const HashData = async (data) => {
+    return bcrypt.hash(data, 10)
+};
+
+export const CompareData = async (data, HashedData) => {
+    return bcrypt.compare(data, HashedData)
+}
